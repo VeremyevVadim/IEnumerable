17678ba2084797648bc8ac5fe9a769b285332d49
VeremyevVadim <qwertyvadimv@mail.ru>
2020-08-25 16:42:25 +0300

    Добавьте файлы проекта.

---
+++ Tree/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Tree
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Add_Btn = new System.Windows.Forms.Button();
+            this.Srch_Btn = new System.Windows.Forms.Button();
+            this.edit_add = new System.Windows.Forms.TextBox();
+            this.Output_Btn = new System.Windows.Forms.Button();
+            this.Log_TxBx = new System.Windows.Forms.TextBox();
+            this.edit_search = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Add_Btn
+            // 
+            this.Add_Btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.Add_Btn.Location = new System.Drawing.Point(12, 326);
+            this.Add_Btn.Name = "Add_Btn";
+            this.Add_Btn.Size = new System.Drawing.Size(127, 33);
+            this.Add_Btn.TabIndex = 0;
+            this.Add_Btn.Text = "Добавить";
+            this.Add_Btn.UseVisualStyleBackColor = true;
+            this.Add_Btn.Click += new System.EventHandler(this.Add_Btn_Click);
+            // 
+            // Srch_Btn
+            // 
+            this.Srch_Btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.Srch_Btn.Location = new System.Drawing.Point(168, 326);
+            this.Srch_Btn.Name = "Srch_Btn";
+            this.Srch_Btn.Size = new System.Drawing.Size(127, 33);
+            this.Srch_Btn.TabIndex = 2;
+            this.Srch_Btn.Text = "Поиск";
+            this.Srch_Btn.UseVisualStyleBackColor = true;
+            this.Srch_Btn.Click += new System.EventHandler(this.Srch_Btn_Click);
+            // 
+            // edit_add
+            // 
+            this.edit_add.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.edit_add.Location = new System.Drawing.Point(12, 283);
+            this.edit_add.Name = "edit_add";
+            this.edit_add.Size = new System.Drawing.Size(127, 27);
+            this.edit_add.TabIndex = 3;
+            // 
+            // Output_Btn
+            // 
+            this.Output_Btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.Output_Btn.Location = new System.Drawing.Point(12, 379);
+            this.Output_Btn.Name = "Output_Btn";
+            this.Output_Btn.Size = new System.Drawing.Size(283, 34);
+            this.Output_Btn.TabIndex = 4;
+            this.Output_Btn.Text = "Обход дерева";
+            this.Output_Btn.UseVisualStyleBackColor = true;
+            this.Output_Btn.Click += new System.EventHandler(this.Output_Btn_Click);
+            // 
+            // Log_TxBx
+            // 
+            this.Log_TxBx.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.Log_TxBx.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.Log_TxBx.Location = new System.Drawing.Point(12, 12);
+            this.Log_TxBx.Multiline = true;
+            this.Log_TxBx.Name = "Log_TxBx";
+            this.Log_TxBx.ReadOnly = true;
+            this.Log_TxBx.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.Log_TxBx.Size = new System.Drawing.Size(283, 265);
+            this.Log_TxBx.TabIndex = 5;
+            // 
+            // edit_search
+            // 
+            this.edit_search.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.edit_search.Location = new System.Drawing.Point(168, 283);
+            this.edit_search.Name = "edit_search";
+            this.edit_search.Size = new System.Drawing.Size(127, 27);
+            this.edit_search.TabIndex = 6;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(308, 425);
+            this.Controls.Add(this.edit_search);
+            this.Controls.Add(this.Log_TxBx);
+            this.Controls.Add(this.Output_Btn);
+            this.Controls.Add(this.edit_add);
+            this.Controls.Add(this.Srch_Btn);
+            this.Controls.Add(this.Add_Btn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Main";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Add_Btn;
+        private System.Windows.Forms.Button Srch_Btn;
+        private System.Windows.Forms.TextBox edit_add;
+        private System.Windows.Forms.Button Output_Btn;
+        private System.Windows.Forms.TextBox Log_TxBx;
+        private System.Windows.Forms.TextBox edit_search;
+    }
+}
+
---
+++ Tree/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Windows.Forms;
+
+namespace Tree
+{
+    public partial class MainForm : Form
+    {
+        public static MainForm TreeForm;
+        BinaryTree Tree;
+
+        public MainForm()
+        {
+            TreeForm = this;
+            Tree = new BinaryTree();
+            Tree.Root = Tree;
+            InitializeComponent();
+
+        }
+
+        // Вывод дерева
+        private void Output_Btn_Click(object sender, EventArgs e)
+        {
+            Log_TxBx.AppendText("Прямой обход:\r\n");
+            foreach (var node in Tree)
+            {
+                Log_TxBx.AppendText($"{node.ToString()} ");
+            }
+            Log_TxBx.AppendText("\r\n");
+        }
+
+        // Действия с узлами
+        private void Add_Btn_Click(object sender, EventArgs e)
+        {
+            int value;
+
+            try
+            {
+                value = int.Parse(edit_add.Text);
+            }
+            catch (System.FormatException)
+            {
+                MessageBox.Show("Введите число.");
+                return;
+            }
+
+            if(Tree.Insert(value) == true)
+                Log_TxBx.AppendText($"Значение {value} добавлено.\r\n");
+            else
+                Log_TxBx.AppendText($"Значение {value} уже существует.\r\n");
+            edit_add.Text = "";
+        }
+
+        private void Srch_Btn_Click(object sender, EventArgs e)
+        {
+            int value;
+            string mes;
+
+            try
+            {
+                value = int.Parse(edit_search.Text);
+            }
+            catch (System.FormatException)
+            {
+                MessageBox.Show("Введите число.");
+                return;
+            }
+
+            var node = Tree.Find(value);
+
+            mes = node == null ? $"Узел со значением {value} не найден." : $"Узел со значением {value} найден.";
+            Log_TxBx.AppendText($"{mes}\r\n");
+            edit_search.Text = "";
+        }
+
+        // Вывод в окно
+        public static void WriteInLog(string text)
+        {
+            TreeForm.Log_TxBx.AppendText($"{text}\r\n");
+        }
+    }
+
+
+    // Бинарное дерево поиска
+    public class BinaryTree : IEnumerable
+    {
+        public int? Data { get; private set; }
+        public BinaryTree Left { get; set; }
+        public BinaryTree Right { get; set; }
+        public BinaryTree Root = null;
+
+
+        // Вставка значения в дерево.
+        public bool Insert(int data)
+        {
+            bool result = false;
+
+            if (Data == null)
+            {
+                Data = data;
+                return true;
+            }
+
+            if (Data > data)
+            {
+                if (Left == null) Left = new BinaryTree();
+                result = Insert(data, Left);
+            }
+            else if(Data < data)
+            {
+                if (Right == null) Right = new BinaryTree();
+                result = Insert(data, Right);
+            }
+
+            return result;
+        }
+
+        // Вставка значения в определённый узел дерева
+        private bool Insert(int data, BinaryTree node)
+        {
+            bool result = false;
+
+            if (node.Data == null)
+            {
+                node.Data = data;
+                return true;
+            }
+            if (node.Data > data)
+            {
+                if (node.Left == null) node.Left = new BinaryTree();
+                result = Insert(data, node.Left);
+            }
+            else if(node.Data < data)
+            {
+                if (node.Right == null) node.Right = new BinaryTree();
+                result = Insert(data, node.Right);
+            }
+
+            return result;
+        }
+
+        // Поиск узла с определенным значением
+        public BinaryTree Find(int data)
+        {
+            if (Data == data) return this;
+            if (Data > data)
+            {
+                return Find(data, Left);
+            }
+            return Find(data, Right);
+        }
+
+        // Поиск значения в определенном узле
+        public BinaryTree Find(int data, BinaryTree node)
+        {
+            if (node == null) return null;
+
+            if (node.Data == data) return node;
+            if (node.Data > data)
+            {
+                return Find(data, node.Left);
+            }
+            return Find(data, node.Right);
+        }
+
+        // Прямой обход дерева с определенного узла.
+        public IEnumerable WalkTree(BinaryTree node)
+        {
+            if (node != null)
+            {
+                if (node.Left != null)
+                    foreach (var elem in WalkTree(node.Left))
+                    {
+                        yield return elem;
+                    }
+
+                yield return node.Data;
+
+                if (node.Right != null)
+                    foreach (var elem in WalkTree(node.Right))
+                    {
+                        yield return elem;
+                    }
+            }
+
+            yield break;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return WalkTree(Root).GetEnumerator();
+        }
+
+        public override string ToString()
+        {
+            return Data.ToString();
+        }
+    }
+}
